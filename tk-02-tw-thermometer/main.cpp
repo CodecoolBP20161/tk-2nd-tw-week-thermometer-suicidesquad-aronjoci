5920114647c2a15dfdfbebf6e6f9e46009618a06
Áron Kékes <kekes.aron@gmail.com>
2017-02-20 11:38:31 +0100

    Added turn_off_leds() method, which turns off all compontent of the RGB led on the shield, and set_led_color_based_on_temp() method with shield led initialization.

--- tk-02-tw-thermometer/main.cpp
+++ tk-02-tw-thermometer/main.cpp
@@ -1,15 +1,25 @@
 #include "mbed.h"
 #include "codecool/codecool_shield_names.h"
 
+void turn_off_leds(gpio_t red_led, gpio_t green_led, gpio_t blue_led){
+	gpio_write(&red_led, 1);
+	gpio_write(&green_led, 1);
+	gpio_write(&blue_led, 1);
+}
+
+void set_led_color_based_on_temp(float temp){
+	gpio_t red;
+	gpio_t green;
+	gpio_t blue;
+
+	gpio_init_out(&red, LED_RED_SHIELD);
+	gpio_init_out(&green, LED_GREEN_SHIELD);
+	gpio_init_out(&blue, LED_BLUE_SHIELD);
+
+}
+
 int main() {
-	gpio_t led;
-
-	gpio_init_out(&led, LED_GREEN_SHIELD);
-
 	while(1) {
-		gpio_write(&led, 1);
-		wait(0.5);
-		gpio_write(&led, 0);
-		wait(0.5);
+		set_led_color_based_on_temp(1.0);
 	}
 }
