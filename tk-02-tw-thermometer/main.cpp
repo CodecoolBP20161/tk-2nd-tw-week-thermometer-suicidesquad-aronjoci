7a6c8b6c2418c982020ea4c2b764c67b78155f67
Swaggerino <bjoci93@gmail.com>
2017-02-21 09:37:02 +0100

    send temperature over uart function implemented

--- tk-02-tw-thermometer/main.cpp
+++ tk-02-tw-thermometer/main.cpp
@@ -1,5 +1,31 @@
 #include "mbed.h"
 #include "codecool/codecool_shield_names.h"
+#include "codecool_i2c.h"
+#include "codecool_serial.h"
+#include "codecool_joystick.h"
+#define LM75_ADDRESS 0x90
+
+void send_temperature_uart(){
+
+	I2C_FREQ(100000);
+
+	uint8_t buffer[16];
+	float temp;
+
+	buffer[0] = 0x00;
+	I2C_WRITE(LM75_ADDRESS, buffer, 1);
+
+	memset(buffer, 0x00, sizeof (buffer)); /* Remove memory trash */
+	I2C_READ(LM75_ADDRESS, buffer, 2);
+
+	int8_t _int_part = (int8_t)buffer[0];
+	temp = _int_part + 0.5f * ((buffer[1]&0x80)>>7); /* save temperature as float */
+
+	char* temp_data;
+    strncpy(temp_data,(char*) temp, 4);
+    SERIAL_SEND(temp_data, 4);
+
+}
 
 int main() {
 	gpio_t led;
@@ -7,9 +33,8 @@ int main() {
 	gpio_init_out(&led, LED_GREEN_SHIELD);
 
 	while(1) {
-		gpio_write(&led, 1);
-		wait(0.5);
-		gpio_write(&led, 0);
-		wait(0.5);
+		if (JOYSTICK_LEFT == 1 ){
+			get_temperature();
+		}
 	}
 }
